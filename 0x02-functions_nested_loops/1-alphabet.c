ef41b6fed9e4964d9d240c6fa3e89952082cbfcd
odu-1985 <odutolamoshood1985@gmail.com>
2022-07-06 11:18:02 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets in small letters
+ *
+ */
+
+void print_alphabet(void)
+{
+
+char alpha = 'a';
+
+
+while (alpha <= 'z')
+{
+_putchar(alpha);
+alpha++;
+
+}
+_putchar('\n');
+}
