a5fb9d0e80f551df7b8c1bace023837b1f6f183a
odu-1985 <odutolamoshood1985@gmail.com>
2022-07-14 05:35:32 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_rev - prints string in reverse
+* @s: The string to be printed
+*/
+void print_rev(char *s)
+{
+	int len = 0, i;
+
+	while (s[len] != '\0')
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar(10);
+}
