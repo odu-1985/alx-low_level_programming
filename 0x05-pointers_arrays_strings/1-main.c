cc486d91ee6f3f8d3dd18025dd2d55ab724fd289
odu-1985 <odutolamoshood1985@gmail.com>
2022-07-14 04:16:31 -0700

    corrected 1-swap.c

--- 0x05-pointers_arrays_strings/1-main.c
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -13,8 +13,8 @@ int main(void)
 
 a = 98;
 b = 42;
-printf(a=%d, b=%d\n, a, b);
+printf("a=%d, b=%d\n", a, b);
 swap_int(&a, &b);
-print("a=%d, b=%d\n", a, b);
+printf("a=%d, b=%d\n", a, b);
 return (0);
 }
