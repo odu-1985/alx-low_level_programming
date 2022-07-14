5465435d7c0552e3df104e9e7154a601e519d220
odu-1985 <odutolamoshood1985@gmail.com>
2022-07-14 04:10:04 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* swap_int - Swap two numbers
+* @a: Pointer variable of one number
+* @b: Pointer variable of two number
+*/
+void swap_int(int *a, int *b)
+{
+      int i;
+
+i = *a;
+*a = *b;
+*b = i;
+
+}
