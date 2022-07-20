a08525b89b0e69598cb926139979f3f75e441e1e
odu-1985 <odutolamoshood1985@gmail.com>
2022-07-20 08:59:58 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,17 +5,13 @@
  * @s: string
  * Return: no return.
  */
-
 void _puts_recursion(char *s)
-
 {
-
 	if (*s != '\0')
 	{
-
-	_putchar(*s);
-        _puts_recursion(s + 1);
-        }
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 	else
 		_putchar('\n');
 }
