fbc65411441a35dce822636a4cda79788f947564
odu-1985 <odutolamoshood1985@gmail.com>
2022-07-26 02:17:27 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - aprogram that prints its name
+* @argc: argument count
+* @argv: argument vector -array of command arguments
+* Return: 0
+*/
+
+int main(int agrc, char *argv[])
+{
+      (void)argv;
+
+      printf("%s\n", argv[0]);
+
+      return (0);
+}
